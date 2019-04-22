ed9f72a35725d435932ed7ed40516bdd278d5c64
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-04-22 10:26:45 +0200

    consulting the API with useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import axios from 'axios';
 
 function App() {
-  return(<p>hola</p>)
+
+  const [frase, obtenerFrase] = useState({});
+
+  const consultarApi = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    //console.log(resultado.data[0]);
+    // agregar el resultado e la API al state (similar a this.setState)
+    obtenerFrase(resultado.data[0])
+  }
+
+  // consulta a una rest api
+  useEffect(
+    () => {
+      consultarApi();
+    }, []
+  )
+
+  console.log(frase); // frase = this.state;
+
+  return <p> hola </p>
 }
 
 export default App;
